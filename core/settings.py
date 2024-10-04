983c2d650dd98769e6111dcb7b6eab28c3167705
RobsonFe <robson_20ferreira@hotmail.com>
2024-10-03 22:53:53 -0300

    update:config

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("NOME_DO_BANCO"),
+        'USER': os.getenv("USUARIO_DO_BANCO"),
+        'PASSWORD': os.getenv("SENHA_DO_BANCO"),
+        'HOST': os.getenv("HOST_DO_BANCO"),
+        'PORT': os.getenv("PORTA_DO_BANCO"),
     }
 }
 
