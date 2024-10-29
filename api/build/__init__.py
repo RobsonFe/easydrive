b4049e93e554bc9f63ad052e1e0cbfac7aa74478
RobsonFe <robson_20ferreira@hotmail.com>
2024-10-29 10:35:54 -0300

    update:imports

--- api/build/__init__.py
+++ api/build/__init__.py
@@ -1,7 +1,11 @@
 from .user_builder import  UserBuilder
 from .client_builder import ClientBuilder
+from .vehicle_builder import VehicleBuilder
+from .rent_builder import RentBuilder
 
 __All__ = [
     "UserBuilder",
-    "ClientBuilder"
+    "ClientBuilder",
+    "VehicleBuilder",
+    "RentBuilder"
 ]
